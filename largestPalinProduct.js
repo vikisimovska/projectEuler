b1c54e6278d386f06ebc934f6837a5071d8f82e6
Humanity Loaner <humanityloaner@ITs-MacBook-Air-4.local>
2015-06-19 10:46:27 -0700

    imlpemented largestPalinProduct.js

---
+++ largestPalinProduct.js
@@ -0,0 +1,29 @@
+/* 
+* @Author: humanityloaner
+* @Date:   2015-06-19 10:36:20
+* @Last Modified by:   humanityloaner
+* @Last Modified time: 2015-06-19 10:45:35
+*/
+
+'use strict';
+
+var checkPalin = function(numInput){
+  var work = numInput.toString();
+  var check = '';
+  work.split('').map(function(value){check = value + check});
+  return (check === work)? true : false;
+}
+
+var getThem = function(){
+  var work = [];
+
+  for (var i = 100; i < 1000; i++){
+    for (var j = 100; j < 1000; j++){
+      if (checkPalin(i * j)){
+        work.push(i*j);
+      }
+    }
+  }
+  return Math.max.apply(this, work);
+}
+
