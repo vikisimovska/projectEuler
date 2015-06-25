f4d083dc04fbab0c32476b6b5e51cd3f3ce791d5
vokoshyv <ronsuij@gmail.com>
2015-06-25 16:13:47 -0700

    initial files for twoPoweredLong.js

---
+++ twoPoweredLong.js
@@ -0,0 +1,14 @@
+// https://projecteuler.net/problem=16
+
+
+// for multiplying string by two
+var multiplyByTwo = function(string){
+  var carryOver = 0;
+  var result = '';
+  var i = string.length; 
+
+  while (i--){
+    console.log(string[i]);
+  }
+
+}
