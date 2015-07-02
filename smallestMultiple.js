96b52e6663a57521356a097954a7b58482031d5a
vokoshyv <ronsuij@gmail.com>
2015-07-02 14:37:56 -0700

    implements solution for smallestMultiple.js

--- smallestMultiple.js
+++ smallestMultiple.js
@@ -1,5 +1,19 @@
 // https://projecteuler.net/problem=5
 
+var check = function(input){
+	var check = true;
+	for (var i = 1; i < 21; i++){
+		if (input % i !== 0){
+			check = false;
+		}
+	}
+	return check;
+}
+
 var lookFor = function(){
-	
+	var count = 1;
+	while (!check(count)){
+		count++;
+	}
+	return count;
 }
