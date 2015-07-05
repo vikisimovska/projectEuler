7249fb908f5301fc915eaa712e02eecd022f8ff9
vokoshyv <ronsuij@gmail.com>
2015-07-05 16:22:20 -0700

    implements solution for sumSquareDifference.js

--- sumSquareDifference.js
+++ sumSquareDifference.js
@@ -1,2 +1,13 @@
 // https://projecteuler.net/problem=6
 
+var toBeSquared = 0; 
+var sumOfSquares = 0;
+
+for (var i = 1; i < 101; i++){
+  toBeSquared += i;
+  sumOfSquares += (i * i);
+}
+
+toBeSquared = toBeSquared * toBeSquared;
+
+console.log(toBeSquared - sumOfSquares);
